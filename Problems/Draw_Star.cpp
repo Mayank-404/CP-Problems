e0f4206288b520f0e276c90f151231ab75248fc8
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-01-25 15:17:52 +0530

    Update Draw_Star.cpp
    Print out the following pattern of stars for n rows.
    
       *
      ***
     *****
    *******

--- Problems/Draw_Star.cpp
+++ Problems/Draw_Star.cpp
@@ -1,10 +1,18 @@
+/** Print out the following pattern of stars for n rows.
+
+   *
+  ***
+ *****
+*******
+
+**/
 #include<bits/stdc++.h>
 
 using namespace std; 
 void stars_vii(int n) {
         for (int i = 1; i <= n; i++) {
         for (int j = n; j > i; j--) {
-            cout << " ";
+            cout << " "; 
         }
         for (int l = 1; l <= i; l++) {
             cout << "*";
