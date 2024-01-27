90f2c2fbeab8bdfa3851d3ad24728a1eb5c17c54
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-01-27 12:23:36 +0530

    Create Draw_ChristmasTree.cpp
    Print out the following pattern of stars for n rows. The bottom of the tree is always 1 star in the middle and n is an odd number
    
         *
        ***
       *****
      *******
     *********
          *

---
+++ Problems/Draw_ChristmasTree.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+void stars_vii(int n) {
+        for (int i = 1; i <= n; i++) {
+        for (int j = n; j > i; j--) {
+            cout << " "; 
+        }
+        for (int l = 1; l <= i; l++) {
+            cout << "*";
+        }
+        for (int k=1;k<i;k++ )
+              cout<<"*";
+        cout << endl;
+        
+    }
+    if (n!=1){
+        for (int i=1; i<=n; i++){
+        if(i!=n)
+        cout<<" ";
+        else
+        cout<<"*";
+        }
+    }
+}
+int main(){
+        int n;
+        cin>>n;
+        stars_vii(n);
+}
