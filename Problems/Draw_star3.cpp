7b7711bbd3f3bce8b10965bdcf30c30761c170a8
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-02-03 20:02:48 +0530

    Create Draw_star3.cpp
     *
     ***
    *****
     ***
      *

---
+++ Problems/Draw_star3.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+void stars_vii(int n) {
+    for (int i = 0; i <n/2+1; i++) {
+        for(int j=0; j<(n/2+1)-i-1; j++){
+            cout<<" ";
+        }
+        for(int l=0; l<2*i+1; l++){
+            cout<<"*";
+        }
+    cout<<endl;
+    }
+    for (int i=0; i<n/2; i++){
+        for (int j=2; j<=i;j++){
+            cout<<" ";
+        }
+        for(int l=n-2-2*i;l>=1;l--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+   
+}
+int main(){
+        int n;
+        cin>>n;
+        stars_vii(n);
+}
