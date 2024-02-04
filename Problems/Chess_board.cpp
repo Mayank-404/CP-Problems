37389618bdc1aa5151a64591cb4cb9830c734c92
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-02-04 14:47:41 +0530

    Create Chess_board.cpp
    *   *   * 
      *   *   
    *   *   * 
      *   *   
    *   *   *

---
+++ Problems/Chess_board.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std; 
+void s_vii(int n) {
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if((j+i)%2==0){
+            cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+    cout<<endl;
+    }
+}
+int main(){
+        int n;
+        cin>>n;
+        s_vii(n);
+}
