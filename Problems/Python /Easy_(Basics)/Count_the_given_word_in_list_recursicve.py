556947d0e84c752de62d180a2c17b3659a66e203
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-05-27 17:59:09 +0530

    Create Count_the_given_word_in_list_recursive.py
    Count the given word in list in recursive way .py

---
+++ Problems/Python /Easy_(Basics)/Count_the_given_word_in_list_recursicve.py
@@ -0,0 +1,12 @@
+def count(L,word):
+    if len(L)==0:
+        return 0
+       
+   
+    elif L[0]==word:
+        return 1+count(L[1:],word)
+    else:
+        return count(L[1:],word)
+L=['abc', 'def', 'abcde', 'abc', 'def', 'abc', 'def', 'ghi', 'abc']
+word=input("Enter ")
+print(count(L,word))
