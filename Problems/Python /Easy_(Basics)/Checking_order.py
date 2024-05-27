bf8bb0b82be57deb09af8e3286e90be16ad624b0
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-05-27 21:17:00 +0530

    Create Checking_order.py
    Write a recursive function named non_decreasing that accepts a non-empty list L of integers as argument and returns True if the elements are sorted in non-decreasing order from left to right, and False otherwise.

---
+++ Problems/Python /Easy_(Basics)/Checking_order.py
@@ -0,0 +1,11 @@
+def non_decreasing(L):
+    if len(L)==1:
+        return True
+    else:
+        if L[0]<=L[1]:
+            return non_decreasing(L[1:])
+        else:
+            return False
+L=[1,2,3,4,7,6]
+
+print(non_decreasing(L))
