a845296fb58e11ddb9b99a476d367aed221241e8
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-06-12 15:36:35 +0530

    Update Counting_txt_file.py
    creating a dictionary that stores the word and  the number of times that word appeared in a txt file as it's key and this is my code

--- Problems/Python /Easy_(Basics)/Counting_txt_file.py
+++ Problems/Python /Easy_(Basics)/Counting_txt_file.py
@@ -1,17 +1,24 @@
 f=open('my.txt','r')
-c=f.read()
-d={}
-print(1)
+# Read the first character from the file
+c=f.read(1)
+# Initialize an empty dictionary to store words and their counts
+p={}
+count=1
+# Loop until the end of the file
 while c!='':
-    count=1
-    e=c
-    while c!=' ':
-        e=e+c
+    d='' # Initialize an empty string to build the current word
+
+   # Build the word until a space or end of file is reache
+    while c != ' ' and c != '':
+        d+=c
         c=f.read(1)
-    if e in d.keys() :
-        count+=1
-    d.update({e:count})
-    print(d) 
-f.close()
-  
-    '''Not Completed Yetttttttttttttttttttttttt'''
+   # Increment the word count if the word is already in the dictionar
+    if d in p:
+        p.update({d:p[d]+1})
+    # Add the word to the dictionary with a count of 1 if it's not already there    
+    else:
+        p.update({d:1})
+        
+    c=f.read(1)
+print(p)
+f.close()# Close the file
