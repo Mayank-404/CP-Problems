a3e982526c6230a25b064b37436baf42581afe4e
Mayank Ks <98734943+Mayank-404@users.noreply.github.com>
2024-06-21 11:30:40 +0530

    Create Class_Calculator.py
    Create a class named Calculator that has the following specification:
    
    Attributes
    
    (1) a: int, we shall call this the first attribute
    
    (2) b: int, we shall call this the second attribute
    
    Methods
    
    (1) __init__: accept two arguments a and b, assign them to the corresponding attributes
    (2) add: return the sum of the two attributes
    (3) multiply: return the product of the two attributes
    (4) subtract: subtract the second attribute from the first and return this value

---
+++ Python /Easy_(Basics)/Class_Calculator.py
@@ -0,0 +1,14 @@
+class Calculator:
+  def __init__ (self, a, b):
+    self.a=a
+    self.b=b
+  def add(self):
+    print(self.a+self.b)
+  def sub(self):
+    print(self.a-self.b)
+  def mul(self):
+    print(self.a*self.b)
+  def div(self):
+    print(self.a/self.b)
+c=Calculator(a,b)
+  
